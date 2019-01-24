8cb72e25ffa13c91053128bd17991189b2a1842b
Eliana <elianalucila@gmail.com>
2019-01-24 17:04:02 -0300

    Update index.js
    Update con async/await y fake REST server

--- index.js
+++ index.js
@@ -1,78 +1,9 @@
-const A = [{
-    "id": 1,
-    "nombre": "Juan",
-    "apellido": "Perez",
-  },
-  {
-    "id": 2,
-    "nombre": "Cosme",
-    "apellido": "Fulanito",
-  },
-];
+const request = async (endpoint) => {
+  let response = await fetch(`https://my-json-server.typicode.com/ElitaTnk/ListadoOrdenado/${endpoint}`);
 
-const B = [
-  {
-    "id": 3,
-    "nombre": "Juan",
-    "apellido": "Perez",
-  },
-  {
-    "id": 4,
-    "nombre": "Jane",
-    "apellido": "Doe",
-  },
-];
-
-const C = [{
-    "personaId": "1",
-    "calle": "Lavalle",
-    "altura": "150",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-    "pais": "Argentina",
-  },
-  {
-    "personaId": "3",
-    "calle": "Av. del Libertador",
-    "altura": "13900",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-    "pais": "Argentina",
-  },
-];
-
-const D = [
-	{
-    "personaId": "2",
-    "calle": "Peron",
-    "altura": "1500",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-    "pais": "Argentina",
-  },
-];
-
-
-const E = [
-  {
-    "codigoPostal": "1047",
-    "calle": "Lavalle",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-  },
-  {
-    "codigoPostal": "1125",
-    "calle": "Peron",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-  },
-  {
-    "codigoPostal": "1047",
-    "calle": "Av. del Libertador",
-    "ciudad": "Buenos Aires",
-    "provincia": "Buenos Aires",
-  },
-];
+  let data = await response.json()
+  return data;
+}
 
 // default
 const F = {
@@ -84,69 +15,90 @@ const F = {
 };
 
 function checkYAgregar(persona, arr) {
-  const found = arr.some(function (el) {
+  const found = arr.some(function(el) {
     return el.nombre === persona.nombre && el.apellido === persona.apellido;
   });
-  if (!found) { arr.push({...persona}); }
+  if (!found) {
+    arr.push({ ...persona
+    });
+  }
 }
 
-function limpiarLista(a, b) {
-	const lista = [...a, ...b];
+
+async function limpiarLista() {
+  const a = await request('listPersonaA');
+  const b = await request('listPersonaB');
+  const lista = [...a, ...b];
   let finalLista = [];
 
-	lista.forEach(function(element) {
-		checkYAgregar(element,finalLista)
+  lista.forEach(function(element) {
+    checkYAgregar(element, finalLista)
   });
-	return finalLista;
+	
+  return finalLista;
 }
 
-function armarListado(personas) {
-  return personas.map(persona => {
-    const direccion = buscarDireccion(persona);
-    const codigoPostal = buscarCP(direccion);
+async function armarListado(personas) {
+  return personas.map(async (persona) => {
+    const direccion = await buscarDireccion(persona);
+    const codigoPostal = await buscarCP(direccion);
 
     direccion.codigoPostal = codigoPostal ? codigoPostal.codigoPostal : null;
+
     direccion.calleAltura = direccion.calle + direccion.altura;
     delete direccion.calle;
     delete direccion.altura;
     delete direccion.personaId;
 
     persona.direccion = direccion;
-
     return persona;
   });
 }
 
-function ordenarPersonas(personas, sortingProperty = "id") {
-  return personas.sort((obj1, obj2) => {
-  	const valueA = obj1[sortingProperty];
-    const valueB = obj2[sortingProperty];
-
-    return (valueA < valueB) ? -1 : (valueA > valueB) ? 1 : 0;
+ function ordenarPersonas(personas, sortingProperty = "id") {
+  return personas.sort( (obj1, obj2) => {
+    const valueA =  obj1[sortingProperty];
+    const valueB =  obj2[sortingProperty];
+	return (valueA < valueB) ? -1 : (valueA > valueB) ? 1 : 0;
   });
 }
 
-function buscarDireccion(persona) {
-  return C.find(p => p.personaId == persona.id) || D.find(p => p.personaId == persona.id) || F;
+async function buscarDireccion(persona) {
+  const c = await request('listaDireccionC');
+  const personInC = c.find(p => p.personaId == persona.id);
+  let personInD = null;
+
+  if (!personInC) {
+    const d = await request('listaDireccionD');
+    personInD = d.find(p => p.personaId == persona.id);
+  }
+
+  return personInC || personInD || F;
 }
 
-function buscarCP(direccion) {
-  return E.find(dir => {
-    	return dir.calle === direccion.calle && dir.ciudad === direccion.ciudad && dir.provincia === direccion.provincia;
-    });
+async function buscarCP(direccion) {
+  const e = await request('listaCP');
+  return e.find(dir => {
+    return dir.calle === direccion.calle && dir.ciudad === direccion.ciudad && dir.provincia === direccion.provincia;
+  });
 }
 
-function generarListado2(sortingProperty) {
-  const personas = limpiarLista(A, B);
-  const personasConDireccion = armarListado(personas);
-  const personasOrdenadas = ordenarPersonas(personasConDireccion, sortingProperty);
+async function generarListado() {
+  const personas = await limpiarLista();
+  const personasConDireccion = await armarListado(personas);
 
-  return personasOrdenadas;
+	return personasConDireccion;
 }
 
 
 
+async function init() {
+  const listado = await generarListado();
 
+  Promise.all(listado).then((data) => {
+  	let personasOrdenadas =  ordenarPersonas(data, "nombre");
+    console.log(JSON.stringify(personasOrdenadas));
+  });
+}
 
-const listado = generarListado2("nombre");
-console.log(JSON.stringify(listado));
+init();
